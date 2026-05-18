Fix the omega rotation bug

rigid_robot/3D_robot/examples/SingleDiskUnderGravity.py
@@ -28,14 +28,14 @@ if __name__ == "__main__":
 
     def control_logic(time):
         # Apply an upward force for the first 5 seconds, then no control input
-        if time >0.0:
+        if time < 4:
             return np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])  
         else:
             return np.zeros(6)  # No control input
         
     simulator = Simulator3D(
         time_step = 0.1,
-        duration = 3.0,
+        duration = 10,
         control_logic = control_logic,  # No control input
         stepper = 'explicit_euler',  
     )
@@ -49,17 +49,21 @@ if __name__ == "__main__":
     time_collection = np.array(simulator.time_collection)
     posture_collection = np.array(simulator.posture_collection)  # Shape: (num_steps, 4, 4)
     position_over_time = posture_collection[:, :3, 3]  # Extract position (x, y, z) over time
+    velocity_matrix_collection = np.array(simulator.velocity_matrix_collection)  # Shape: (num_steps, 4, 4)
+    momentum_collection = np.array(simulator.momentum_collection)  # Shape: (num_steps
+    omega_x, omega_y, omega_z = velocity_matrix_collection[:, 2, 1], velocity_matrix_collection[:, 0, 2], velocity_matrix_collection[:, 0, 1]  # Extract angular velocities over time  
     position_colleiton_x = position_over_time[:, 0]
     position_colleiton_y = position_over_time[:, 1]
     position_colleiton_z = position_over_time[:, 2]
-    theta = np.array([R.from_matrix(posture_collection[i, :3, :3]).as_euler('xyz') for i in range(len(posture_collection))])  # Extract orientation (roll, pitch, yaw) over time
-    theta_x = theta[:, 0]
-    theta_y = theta[:, 1]
-    theta_z = theta[:, 2]
-
+    
+    theta_z = [0.0]
+    for i in range(len(omega_z)-1):
+        theta_z.append(theta_z[i]+ omega_z[i] * 0.01 )
+    theta_z = np.array(theta_z)
+    
     # Visualization
     plt.figure(figsize=(10, 6))
-    plt.plot(time_collection,theta, label='Z Rotation (Yaw)')
+    plt.plot(time_collection,omega_z, label='Z Rotation (Yaw)')
     plt.title('Z Rotation of the Robot Over Time')
     plt.xlabel('Time (s)')
     plt.ylabel('Z Rotation (rad)')

rigid_robot/3D_robot/robot3d/RigidRobot3D.py
@@ -33,6 +33,9 @@ class RigidRobot3D:
         """
 
         self.radius = radius
+        self.orientation = orientation
+        # Since the posture matrix can no longer correctly track the constitution relationship
+        # after the robot rotate 360 degrees. So wee need a true omega tracking twist over time
         self.thickness = thickness
         self.posture = self.compute_posture(position, orientation)
         self.velocity_matrix = self.compute_velocity_matrix(linear_velocity, angular_velocity)
@@ -67,7 +70,7 @@ class RigidRobot3D:
         spring_anchor_point=np.array([0.0, 0.0, 0.0]), 
         spring_stiffness=1,
         torque_spring_anchor_orientation=np.diag([1.0, 1.0, 1.0]),
-        torque_spring_stiffness=1,
+        torque_spring_stiffness=0.0001,
         spring_original_length=0.04
         ):
 
@@ -101,11 +104,15 @@ class RigidRobot3D:
        
         #TODO: Add torque spring
         # Torque spring will generate a resistanct torque in all three axes to resist changes in orientation
-        theta = R.from_matrix(Q_error).as_rotvec()
+        theta = self.orientation.copy()
         print(f"theta: {theta}")
-        print(f"omega: {omega}")
-        print(f"momentum: {self.momentum}")
-        tau_x, tau_y, tau_z = - torque_spring_stiffness * theta - damping_coefficient * omega
+        #print(f"omega: {omega}")
+        #print(f"momentum: {self.momentum}")
+        tau_x, tau_y, tau_z = - torque_spring_stiffness * theta  
+        print(tau_z)
+        #tau_x, tau_y, tau_z = np.zeros(3)  
+        
+        
  
         # Add control Inputs (all control inputs are applied as the force or toque in local framework)
         total_force_local = np.array([f_x, f_y, f_z, tau_x, tau_y, tau_z]) + control_input
@@ -134,7 +141,6 @@ class RigidRobot3D:
             
         return False  # No contact detected
     
-
 
 class ConnectedRigidRobots3D:
     def __init__(self, robot1: RigidRobot3D, robot2: RigidRobot3D):

rigid_robot/3D_robot/robot3d/Simulator3D.py
@@ -1,6 +1,8 @@
 import numpy as np 
 from .methods3D import SE3LieAlgebra
 import matplotlib.pyplot as plt
+from scipy.spatial.transform import Rotation as R
+
 lie3 = SE3LieAlgebra()
 
 class Simulator3D:
@@ -15,7 +17,10 @@ class Simulator3D:
         self.velocity_matrix_collection = []  # To store robot velocities over time
         self.force_collection = []  # To store forces over time
         self.momentum_collection = []  # To store momentum over time
-        
+        # Since the posture matrix can no longer correctly track the constitution relationship
+        # after the robot rotate 360 degrees. So wee need a true omega tracking twist over time
+
+
     def run(self):
         if len(self.time_collection) * self.time_step >= self.duration:
             return False  # Simulation finished
@@ -37,11 +42,17 @@ class Simulator3D:
 
         if self.stepper == 'explicit_euler':
             # Kinematics: T_{k+1} = T_k @ exp(ξ_k · dt)
+            #print(f"posture_k:\n {posture_k}")
             posture_kp1 = posture_k @ lie3.exp(velocity_k_matrix * self.time_step)
+
+            #print(f"exp(velocity_k_matrix * self.time_step):\n {lie3.exp(velocity_k_matrix * self.time_step)}")
+            #print(f"velocity_k_matrix:\n {velocity_k_matrix}")
 
             # Euler-Poincaré: μ_{k+1} = μ_k + (coad(V_k) · μ_k + F_k) · dt
             #TODO: Is this correct or not?
             momentum_kp1 = (lie3.exp_adjoint(-velocity_k_matrix*self.time_step) @ momentum_k) + (force_k * self.time_step)
+            #print(f"momentum_kp1: {momentum_kp1}")
+            #print("\n")
 
         # -------------------Symplectic Euler Integration -------------------
         if self.stepper == 'symplectic_euler':
@@ -68,6 +79,9 @@ class Simulator3D:
         self.robot[0].posture = posture_kp1
         self.robot[0].momentum = momentum_kp1
         self.robot[0].velocity_matrix = lie3.hat(xi_kp1)
+        velocity_temp = np.array([self.robot[0].velocity_matrix[2, 1], self.robot[0].velocity_matrix[0, 2], self.robot[0].velocity_matrix[1, 0]]) # Angular Velocity
+        delta_orientation = velocity_temp * self.time_step
+        self.robot[0].orientation = self.robot[0].orientation + delta_orientation
 
     def record(self):
         self.time_collection.append(len(self.time_collection) * self.time_step)

rigid_robot/3D_robot/robot3d/methods3D.py
@@ -180,10 +180,10 @@ class SE3LieAlgebra:
    
         v = Xi[:3, 3]
         W = Xi[:3, :3]  # hat(w), skew-symmetric
+        W2 = W @ W
        
-        W_negative = -W
-        W2 = W_negative @ W_negative
-        theta = np.sqrt(W_negative[2, 1]**2 + W_negative[0, 2]**2 + W_negative[1, 0]**2)
+ 
+        theta = np.sqrt(W[2, 1]**2 + W[0, 2]**2 + W[1, 0]**2)
         s, c = np.sin(theta), np.cos(theta)
 
         if theta < self._EPS:
@@ -192,12 +192,15 @@ class SE3LieAlgebra:
             R   = np.eye(3) + ((1 - c) / theta**2) * W + ((theta - s) / theta**3) * W2
 
         exp_ad = np.zeros((6, 6))
-        exp_ad[:3, :3] =  self.exp_SO3(-W)
+        exp_ad[:3, :3] =  self.exp_SO3(W)
 
-        exp_ad[3:, :3] = self.exp_SO3(-W) @ R @ (-self._skew(v))
-        exp_ad[3:, 3:] = self.exp_SO3(-W)
+        exp_ad[3:, :3] = self.exp_SO3(W) @ R @ (self._skew(v))
+        exp_ad[3:, 3:] = self.exp_SO3(W)
+
+        #print(f"exp_adjoint:\n {exp_ad}")
 
         return exp_ad
+    
 
     # ------------------------------------------------------------------ #
     # Convenience                                                          #